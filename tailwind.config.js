d27ea31abd4961f9bd13556f3feaf3af5c0e9b2e
오상훈 <admin@osanghun-ui-MacBookAir.local>
2025-03-14 15:54:47 +0900

    feat: Pretendard font 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Pretendard', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+        'pretendard': ['Pretendard', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
+      },
+    },
   },
   plugins: [],
 }
