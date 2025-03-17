7a45fd6c20be4aa28b70810db9e508545f3795b6
오상훈 <admin@osanghun-ui-MacBookAir.local>
2025-03-17 23:41:34 +0900

    chore: color 설정 extend안으로 이동

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,10 +15,10 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      'fc': '#E4E4E5',
-    },
     extend: {
+      colors: {
+        'fc': '#E4E4E5',
+      },
       fontFamily: {
         'sans': ['Pretendard Variable', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
         'pretendard': ['Pretendard Variable', 'sans-serif'],
