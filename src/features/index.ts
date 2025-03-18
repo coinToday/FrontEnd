543554916dc102c87d01745e59ff68690de49243
yun yeo joung <da.young.yun13@gmail.com>
2025-03-18 16:40:13 +0900

    Revert "feat: 거래 페이지 기능 추가 "

--- src/features/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Trade } from "./Trades/ui/index";
-export { default as OrderBook } from "./OrderBook/ui/index";
-export { default as CandleChart } from "./CandleChart/ui/index";
-export { default as CoinList } from "./CoinList/ui/index";
--- src/shared/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as useWebsocket } from "./hooks/useWebsocket";
